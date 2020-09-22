b6c79895d27dd213a86cc42067f5a97ee48cffe0
ppolleppolle <35333089+cgin1202@users.noreply.github.com>
2020-09-22 22:38:27 +0900

    update secret key

--- webservice/webservice/settings.py
+++ webservice/webservice/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5c0h(0vrywlos)jvpw4e!pui&njgmzzvd34d#dd$2sqt00)9dn'
+SECRET_KEY = '########################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
